45ece2b06bbe7f3f16a30e801184062a70b9fcbb
juragan360 <ariefda@hotmail.com>
2015-04-28 19:55:35 +0700

    add mqtable index

--- msg/table.go
+++ msg/table.go
@@ -11,6 +11,7 @@ type MqTable struct {
 	LastAccess time.Time
 	Expiry     time.Duration
 	Items      map[string]interface{}
+	Indexes    map[string]string
 }
 
 func NewTable(tableid string, owner string) *MqTable {
@@ -18,5 +19,25 @@ func NewTable(tableid string, owner string) *MqTable {
 	ret.TableId = tableid
 	ret.Owner = owner
 	ret.Created = time.Now()
+	ret.Items = make(map[string]interface{})
+	ret.Indexes = make(map[string][]string)
 	return ret
+}
+
+func (t *MqTable) RunIndex(indexname string, indexFunction func(d) string) error {
+	indexes := make(map[string]string)
+	for k, v := range t.Items {
+		indexKey := indexFunction(v)
+		_, e := indexes[k]
+		if !e {
+			indexes[indexKey] = make([]string, 0)
+		}
+		indexes[indexKey] = append(indexes[indexKey], k)
+	}
+	t.Indexes[indexname] = indexes
+	return nil
+}
+
+func (t *MqTable) DropIndex(indexname string) {
+	remove(t.Indexes[indexname])
 }
