1050d9c691b3e3b2abbb1baed80c2fb347fdd08b
nbprayuga <nbprayuga@digdaya.com>
2015-06-18 15:29:50 +0700

    add CheckToken function and finishing GetToken authtentification process in mqapi.go

--- mqapi.go
+++ mqapi.go
@@ -12,6 +12,12 @@ import (
 	"github.com/go-martini/martini"
 )
 
+type User struct {
+	Username string
+	Token    string
+	Valid    time.Time
+}
+
 type TokenData struct {
 	Token string    `json:"token"`
 	Valid time.Time `json:"valid"`
@@ -19,26 +25,92 @@ type TokenData struct {
 
 const (
 	tokenLength = 32
+	expiredTime = 15
+)
+
+var (
+	users []User
+	user  User
 )
 
 func main() {
 	m := martini.Classic()
-	m.Get("/api/gettoken/username=(?P<name>[a-zA-Z0-9]+)&password=(?P<password>[a-zA-Z0-9]+)", GetToken)
+	m.Post("/api/gettoken/username=(?P<username>[a-zA-Z0-9]+)&password=(?P<password>[a-zA-Z0-9]+)", GetToken)
+	m.Post("/api/checktoken/tokenkey=(?P<tokenkey>[a-zA-Z0-9=]+)", CheckToken)
 	m.RunOnAddr(":8090")
 }
 
-func GetToken(w http.ResponseWriter, params martini.Params) string {
-	var result string
-	auth, e := Auth(params["name"], params["password"])
+func CheckToken(w http.ResponseWriter, params martini.Params) {
+	data := TokenData{}
+	isTokenExist := false
+	for _, v := range users {
+		if params["tokenkey"] == v.Token {
+			isTokenExist = true
+			data.Token = v.Token
+			data.Valid = v.Valid
+			PrintJSON(w, true, data, "")
+			break
+		}
+	}
+	if !isTokenExist {
+		PrintJSON(w, false, "", "token doesn't exist")
+	}
+
+}
+
+func GetToken(w http.ResponseWriter, params martini.Params) {
+	username := params["username"]
+	password := params["password"]
+	auth, e := Auth(username, password)
 	if auth && e == nil {
+		token := GenerateRandomString(tokenLength)
+		valid := time.Now().Add(expiredTime * time.Minute)
+		user.Username = username
+		user.Token = token
+		user.Valid = valid
+		if CheckExistedUser(username, valid) {
+			if CheckExpiredToken(user) {
+				user.Token = token
+				user.Valid = valid
+				UpdateTokenAndValidTime(user)
+			}
+		} else {
+			users = append(users, user)
+		}
 		data := TokenData{}
-		data.Token = GenerateRandomString(tokenLength)
-		data.Valid = time.Now().Add(15 * time.Minute)
+		data.Token = user.Token
+		data.Valid = user.Valid
 		PrintJSON(w, true, data, "")
 	} else {
 		PrintJSON(w, false, "", "wrong username and password combination")
 	}
-	return result
+}
+
+func UpdateTokenAndValidTime(usr User) {
+	for k, v := range users {
+		if usr.Username == v.Username {
+			users[k].Token = usr.Token
+			users[k].Valid = usr.Valid
+			break
+		}
+	}
+}
+
+func CheckExistedUser(username string, valid time.Time) bool {
+	isExist := false
+	for k, v := range users {
+		if v.Username == username {
+			users[k].Valid = valid
+			user = users[k]
+			isExist = true
+			break
+		}
+	}
+	return isExist
+}
+
+func CheckExpiredToken(usr User) bool {
+	return usr.Valid.Before(time.Now())
 }
 
 func Auth(username, password string) (bool, error) {
@@ -49,7 +121,6 @@ func Auth(username, password string) (bool, error) {
 	if e != nil {
 		return false, e
 	}
-
 	if i.Value.(ClientInfo).IsLoggedIn {
 		isLoggedIn = true
 	}
